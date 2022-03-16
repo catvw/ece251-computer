7c46a64abfedd686a84d91b56b339824efcc7e59
C. R. Van West <charles@vanwestco.com>
2022-03-16 15:37:22 -0400

    Add printout

--- alu/alu_tb.v
+++ alu/alu_tb.v
@@ -7,23 +7,24 @@
 module alu_tb;
 	reg[7:0] A, B;
 	reg[2:0] S;
+	wire[7:0] D;
+	wire C;
 
 	initial begin
 		$dumpfile("test.vcd");
 		$dumpvars(0, A, B, S, D, C);
+		$display("S              A                     B                     D                     C");
+		$monitor("%b (%d) (0x%0h); %b (%d) (0x%0h); %b (%d) (0x%0h); %b (%d) (0x%0h); %b (%d) (0x%0h)",
+			S, S, S, A, A, A, B, B, B, D, D, D, C, C, C);
 	end
-
-	wire[7:0] D;
-	wire C;
 
 	alu test_alu(A, B, S, D, C);
 	initial begin
-		// hold reset state for 100 ns.
 		A = 8'h02;
 		B = 8'h01;
 		S = 3'h0;
 
-		for (integer i = 0; i <= 7; ++i) begin
+		for (integer i = 0; i <= 6; ++i) begin
 			#10 S = S + 8'h01;
 		end
 	end

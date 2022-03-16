d8a33f74a6eebe199f3252c6c84438c5679a86dc
C. R. Van West <charles@vanwestco.com>
2022-03-16 19:20:11 -0400

    Add ad-hoc program loading code

--- ctrl/ctrl_tb.v
+++ ctrl/ctrl_tb.v
@@ -4,22 +4,28 @@
 module ctrl_tb;
 	reg clock;
 
-	wire mem_clock;
-	wire mem_write;
+	reg mem_clock;
+	reg mem_write;
 
-	wire[7:0] address, from_mem, to_mem;
+	reg[7:0] address, to_mem;
+	wire[7:0] from_mem;
+	wire[7:0] ctrl_addr_out, ctrl_to_mem;
+	reg[7:0] ctrl_from_mem;
+	wire ctrl_mem_clock;
+	wire ctrl_mem_write;
 
 	initial begin
-		$dumpfile("test.vcd");
-		$dumpvars(0, clock, write, address, to_mem, from_mem);
+//		$dumpfile("test.vcd");
+//		$dumpvars(0, clock, write, address, to_mem, from_mem);
 //		$display("clock   write   address to_mem  from_mem");
 //		$monitor("#%d:     %b;      0x%h;   0x%h;   0x%h",
 //			clock, write, address, to_mem, from_mem);
 //		$monitor("#%4d: %b", $time, clock);
 	end
 
-	ctrl test_ctrl(clock, address, from_mem, to_mem, mem_clock, mem_write);
-	mem test_mem(mem_clock, write, address, to_mem, from_mem);
+	ctrl test_ctrl(clock, ctrl_addr_out, ctrl_from_mem, ctrl_to_mem,
+		ctrl_mem_clock, ctrl_mem_write);
+	mem test_mem(mem_clock, mem_write, address, to_mem, from_mem);
 	always #1 clock = !clock;
 
 	integer program_file;
@@ -29,13 +35,39 @@ module ctrl_tb;
 		assign clock = 0; // hold clock at zero until we're ready
 
 		// read the program into memory
-		$monitor("%b (%d)", next_instr, next_instr);
+		$monitor("0x%h: %b (%d)", address, next_instr, next_instr);
 		next_addr = 8'b0;
 		program_file = $fopen("program.bin", "r");
+		mem_write = 1;
+		assign to_mem = next_instr;
+		assign address = next_addr;
 		while ($fread(next_instr, program_file)) begin
-			#1 ++next_addr;
+			// cycle the memory clock and move to the next address
+			mem_clock = 1;
+			mem_clock = 0;
+			#1; // print here and finish assignments
+			++next_addr;
 		end
+		deassign to_mem;
+		deassign address;
 		$fclose(program_file);
+
+		$display("(read-back)");
+
+		mem_write = 0;
+		assign next_instr = from_mem;
+		for (address = 0; address < 8'h20; ++address) begin
+			mem_clock = 1;
+			mem_clock = 0;
+			#1; // print here and finish assignments
+		end
+
+		// hook up memory and control
+		assign mem_clock = ctrl_mem_clock;
+		assign mem_write = ctrl_mem_write;
+		assign address = ctrl_addr_out;
+		assign to_mem = ctrl_to_mem;
+		assign ctrl_from_mem = from_mem;
 
 		deassign clock; // and we're away!
 		#20 $finish;

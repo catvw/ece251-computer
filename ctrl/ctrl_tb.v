d82cf068a2a4b213303a6cd45aad9e81905b13ce
Charles Van West <charles@vanwestco.com>
2022-04-16 19:41:02 -0400

    Read memory in mem.v instead

--- ctrl/ctrl_tb.v
+++ ctrl/ctrl_tb.v
@@ -1,4 +1,7 @@
 `timescale 1ms / 1ms
+
+`define MEMFILE "program.bin"
+
 `include "../mem/mem.v"
 `include "../alu/alu.v"
 `include "../mult/mult.v"
@@ -7,15 +10,11 @@
 module ctrl_tb;
 	reg clock;
 
-	reg mem_clock;
-	reg mem_write;
-
-	reg[7:0] address, to_mem;
+	wire mem_clock;
+	wire mem_write;
+	wire[7:0] address;
+	wire[7:0] to_mem;
 	wire[7:0] from_mem;
-	wire[7:0] ctrl_addr_out, ctrl_to_mem;
-	reg[7:0] ctrl_from_mem;
-	wire ctrl_mem_clock;
-	wire ctrl_mem_write;
 
 	wire[7:0] A, B, D;
 	wire[2:0] S;
@@ -44,43 +43,26 @@ module ctrl_tb;
 	ctrl test_ctrl(
 		clock,
 
-		ctrl_addr_out,
-		ctrl_from_mem,
-		ctrl_to_mem,
-		ctrl_mem_clock,
-		ctrl_mem_write
+		address,
+		from_mem,
+		to_mem,
+		mem_clock,
+		mem_write
 	);
 
-	mem test_mem(mem_clock, mem_write, address, to_mem, from_mem);
+	mem test_mem(
+		mem_clock,
+		mem_write,
+		address,
+		to_mem,
+		from_mem
+	);
 	always #20 clock = !clock;
 
 	integer program_file;
 	reg[7:0] next_instr;
 	reg[7:0] next_addr;
 	initial begin
-		// read the program into memory
-		next_addr = 8'b0;
-		program_file = $fopen("program.bin", "r");
-		mem_write = 1;
-		assign to_mem = next_instr;
-		assign address = next_addr;
-		while ($fread(next_instr, program_file)) begin
-			// cycle the memory clock and move to the next address
-			mem_clock = 1;
-			#1 mem_clock = 0;
-			#1 ++next_addr;
-		end
-		deassign to_mem;
-		deassign address;
-		$fclose(program_file);
-
-		// hook up memory and control
-		assign mem_clock = ctrl_mem_clock;
-		assign mem_write = ctrl_mem_write;
-		assign address = ctrl_addr_out;
-		assign to_mem = ctrl_to_mem;
-		assign ctrl_from_mem = from_mem;
-
 		clock <= 1; // and we're away!
 	end
 endmodule

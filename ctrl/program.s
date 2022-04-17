12d5d8c904916fa5d1b696eee6f9e1d1bbb3b478
Charles Van West <charles@vanwestco.com>
2022-04-16 20:11:42 -0400

    Shorten test program a bit

--- ctrl/program.s
+++ ctrl/program.s
@@ -6,19 +6,21 @@
 
 forward:
 	// set r0 to 1
-	set #0
-	lsl #4
+	//set #0
+	//lsl #4
 	set #1 // i should really have sel/seh instructions...
 	mov >r0
 
-	// loop four times
-	set #4
+	// loop for a sec
+	set #2
 loop:
 	sub r0
 	bnn loop
 
 	add r0
 	bz end
-
+	no
+	no
+	no
 end:
 	hlt

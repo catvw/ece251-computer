dee70d54d5fd6a798d0aa843be00af809fc00f61
Charles Van West <charles@vanwestco.com>
2022-05-28 01:08:15 -0400

    Add license to assembly for lols

--- ctrl/program.s
+++ ctrl/program.s
@@ -91,3 +91,18 @@ recurse:
 
 	ld [r1]			// retrieve return address (as that was the item)
 	ba r5			// branch back to caller!
+
+// Copyright (C) 2022, C. R. Van West
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
